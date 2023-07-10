58f36397540807cca1f6b87461d6553b47d57461
GoliasVictor <dev.jvsb@gmail.com>
2023-07-10 06:49:47 -0300

    feat: implementation find_environment

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use commands::Commands;
 
 
 #[derive(Parser, Debug)]
-#[command(name = "git")]
+#[command(name = "pm")]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[command(subcommand)]
@@ -27,13 +27,22 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    if let Commands::List { flat: _, r#type: _, max_depth: _ } = args.command {
-        let enviroments = mapper::map_directory(&args.root.expect(""));
-        list(enviroments, "".to_string())
+    match args.command {
+        Commands::List { flat: _, r#type: _, max_depth: _ } => {
+            let enviroments = mapper::map_directory(&args.root.expect(""));
+            list(enviroments, "".to_string())
+        },
+        Commands::Dir { project } => {
+            if let Some(env) = mapper::find_environment(&args.root.unwrap(), &project) {
+                println!("{}", env.source.display() );
+            } 
+        }
+        _ => (),
     }   
 }
 fn list(enviroments : Vec<Environment>, prefix : String){
     for env in enviroments {
+        println!("{}{}",prefix, env.name);
         list(env.children,"-".to_string() + &prefix );
     }
 }
--- src/mapper.rs
+++ src/mapper.rs
@@ -1,9 +1,8 @@
-use std::{path::{Path}, fs::{read_dir}};
+use std::{path::{Path}, fs::{read_dir, DirEntry}};
 
 use crate::{providers, enviroment::{Environment, EnvironmentDetails}, EnvironmentType};
 
 fn name_else_filename(opname : Option<String>, path : &Path) -> Option<String> {
-	println!("{:?}",path);
 	if let Some(name) = opname {
 		if !name.trim().is_empty(){
 			return Some(name);
@@ -12,20 +11,22 @@ fn name_else_filename(opname : Option<String>, path : &Path) -> Option<String> {
 	return path.file_name().and_then(|s|s.to_str()).map(|s| s.to_string());
 	
 }
-pub fn map_directory(path : &Path) -> Vec<Environment> { 
-	let mut enviroments : Vec<Environment> =  vec![];
-	
-	let dirs = read_dir(path)
+pub fn get_dirs(path : &Path) -> impl IntoIterator<Item = DirEntry>{
+	return read_dir(path)
 		.unwrap()
 		.filter_map(Result::ok)
 		.filter(|entry| {
 			entry.metadata().unwrap().is_dir()
 		});
-	for dir in dirs {
+}
+pub fn map_directory(path : &Path) -> Vec<Environment> { 
+	let mut enviroments : Vec<Environment> =  vec![];
+
+	for dir in get_dirs(path) {
 
 		if let Some(mut meta) = providers::get_meta(&dir.path()){
 			meta.source = dir.path();
-			//meta.name = name_else_filename(meta.name.clone(), &dir.path());
+			meta.name = name_else_filename(meta.name.clone(), &dir.path());
 			if let Some(EnvironmentType::Project) = meta.environment_type {
 				if let Some(children) = &mut meta.children {
 					for child in children.into_iter() {
@@ -35,7 +36,6 @@ pub fn map_directory(path : &Path) -> Vec<Environment> {
 						child.environment_type = Some(EnvironmentType::SubProject);
 					} 
 				}
-				println!("{:#?}",meta);
 			}
 			if let Some(mut env) = Environment::from_metadata(meta).ok() {
 				if let EnvironmentDetails::Folder = env.details {
@@ -46,4 +46,33 @@ pub fn map_directory(path : &Path) -> Vec<Environment> {
 		}
 	}
 	return enviroments;
+}
+
+pub fn find_environment(path : &Path, name : &str) -> Option<Environment> {
+	let name = name.to_lowercase();
+	let mut folders : Vec<Environment> = vec![] ; 
+	for dir in  get_dirs(path) {
+		if let Some(mut meta) = providers::get_meta(&dir.path()) {
+			meta.source = dir.path();
+			meta.name = name_else_filename(meta.name.clone(), &dir.path());
+
+			if let Some(mut env) = Environment::from_metadata(meta).ok() {
+				if let EnvironmentDetails::Folder = env.details {
+					env.children = map_directory(&env.source);
+				}
+				
+				if env.name.to_lowercase() ==  name {
+					return Some(env);
+				}
+				folders.push(env)
+			}
+		}
+	}
+	for folder in folders {
+		if let Some(env) = find_environment(&folder.source, &name){
+			return Some(env);
+		}
+	}
+
+	return None;
 }
