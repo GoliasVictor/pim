ed63cd16232b82041f1c80fdf06cccb83e97670f
GoliasVictor <dev.jvsb@gmail.com>
2023-07-11 06:08:20 -0300

    refactor: created get_environment

--- src/mapper.rs
+++ src/mapper.rs
@@ -2,17 +2,7 @@ use crate::prelude::*;
 use crate::providers;
 use std::fs::{read_dir, DirEntry};
 
-fn name_else_filename(opname: Option<String>, path: &Path) -> Option<String> {
-    if let Some(name) = opname {
-        if !name.trim().is_empty() {
-            return Some(name);
-        }
-    }
-    return path
-        .file_name()
-        .and_then(|s| s.to_str())
-        .map(|s| s.to_string());
-}
+
 pub fn get_dirs(path: &Path) -> impl IntoIterator<Item = DirEntry> {
     return read_dir(path)
         .unwrap()
@@ -23,29 +13,11 @@ pub fn map_directory(path: &Path) -> Vec<Environment> {
     let mut enviroments: Vec<Environment> = vec![];
 
     for dir in get_dirs(path) {
-        if let Some(mut meta) = providers::get_meta(&dir.path()) {
-            meta.source = dir.path();
-            meta.name = name_else_filename(meta.name.clone(), &dir.path());
-            if let Some(EnvironmentType::Project) = meta.environment_type {
-                if let Some(children) = &mut meta.children {
-                    for child in children.into_iter() {
-                        if let Some(child_path) = child
-                            .path
-                            .as_ref()
-                            .and_then(|p| meta.source.join(&p).canonicalize().ok())
-                        {
-                            child.name = name_else_filename(child.name.clone(), &child_path);
-                        }
-                        child.environment_type = Some(EnvironmentType::SubProject);
-                    }
-                }
+        if let Some(mut env) = providers::get_environment(&dir.path()) {
+            if let EnvironmentDetails::Folder = env.details {
+                env.children = map_directory(&env.source);
             }
-            if let Some(mut env) = Environment::from_metadata(meta).ok() {
-                if let EnvironmentDetails::Folder = env.details {
-                    env.children = map_directory(&env.source);
-                }
-                enviroments.push(env);
-            }
+            enviroments.push(env);
         }
     }
     enviroments.sort_by(|a, b| a.name.cmp(&b.name));
@@ -56,22 +28,17 @@ pub fn find_environment(path: &Path, name: &str) -> Option<Environment> {
     let name = name.to_lowercase();
     let mut folders: Vec<Environment> = vec![];
     for dir in get_dirs(path) {
-        if let Some(mut meta) = providers::get_meta(&dir.path()) {
-            meta.source = dir.path();
-            meta.name = name_else_filename(meta.name.clone(), &dir.path());
-
-            if let Some(mut env) = Environment::from_metadata(meta).ok() {
-                if let EnvironmentDetails::Folder = env.details {
-                    env.children = map_directory(&env.source);
-                }
-
-                if env.name.to_lowercase() == name {
-                    return Some(env);
-                }
-                folders.push(env)
+        if let Some(mut env) = providers::get_environment(&dir.path()){
+            if let EnvironmentDetails::Folder = env.details {
+                env.children = map_directory(&env.source);
             }
+
+            if env.name.to_lowercase() == name {
+                return Some(env);
+            }
+            folders.push(env)
         }
-    }
+    };
     for folder in folders {
         if let Some(env) = find_environment(&folder.source, &name) {
             return Some(env);
@@ -84,17 +51,11 @@ pub fn find_environment(path: &Path, name: &str) -> Option<Environment> {
 pub fn find_parent_environment(path: &PathBuf) -> Option<Environment> {
     let mut path = path.clone();
     loop {
-        if let Some(mut meta) = providers::get_meta(&path){
-            meta.source = path.clone();
-            meta.name = name_else_filename(meta.name.clone(), &path);
-            if let Some(env) =  Environment::from_metadata(meta).ok() {
-                return Some(env);
-            }
+        if let Some(env) = providers::get_environment(&path){
+            return Some(env);
         };
         if !path.pop(){
             return None;
         };
     }
-
-
 }
--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -2,6 +2,40 @@ use crate::prelude::*;
 mod dotmeta_provider;
 mod vscode_provider;
 
-pub fn get_meta(path: &Path) -> Option<Metadata> {
-    return dotmeta_provider::get_meta(path).or(vscode_provider::get_meta(path));
+fn name_else_filename(opname: Option<String>, path: &Path) -> Option<String> {
+    if let Some(name) = opname {
+        if !name.trim().is_empty() {
+            return Some(name);
+        }
+    }
+    return path
+        .file_name()
+        .and_then(|s| s.to_str())
+        .map(|s| s.to_string());
+}
+fn get_meta(path: &Path) -> Option<Metadata> {
+    let mut op =  dotmeta_provider::get_meta(path).or(vscode_provider::get_meta(path));
+    if let Some(ref mut meta) =  op {
+        meta.source = path.to_path_buf();
+        meta.name = name_else_filename(meta.name.clone(), &path);
+        if let Some(EnvironmentType::Project) = meta.environment_type {
+            if let Some(children) = &mut meta.children {
+                for child in children.into_iter() {
+                    if let Some(child_path) = child
+                        .path
+                        .as_ref()
+                        .and_then(|p| meta.source.join(&p).canonicalize().ok())
+                    {
+                        child.name = name_else_filename(child.name.clone(), &child_path);
+                    }
+                    child.environment_type = Some(EnvironmentType::SubProject);
+                }
+            }
+        }
+    }
+    return op;
+}
+
+pub fn get_environment(path: &Path) -> Option<Environment>{
+    return get_meta(path).and_then(|m|Environment::from_metadata(m).ok())
 }
